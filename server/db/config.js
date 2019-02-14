14f92a7c76ffe67e032618a71a01af4ba596fe4d
aeginidae <aeginidae@gmail.com>
2019-02-14 11:57:45 -0600

    define User and Event models

--- server/db/config.js
+++ server/db/config.js
@@ -6,3 +6,37 @@ const sequelize = new Sequelize('hobbyist', process.env.DB_USER, process.env.DP_
   host: process.env.DB_HOST,
   dialect: 'postgres'
 });
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: Sequelize.STRING,
+  password: Sequelize.STRING // needs hashing
+});
+
+const Event = sequelize.define('event', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  category: Sequelize.STRING,
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  time: Sequelize.DATE,
+  lat: Sequelize.NUMERIC,
+  long: Sequelize.NUMERIC,
+  id_user: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+      deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+    }
+  }
+});
+
+sequelize.sync();
