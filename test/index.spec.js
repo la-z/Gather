0aede73d8734a84d411cb8a4248c7f5772c18e75
aeginidae <aeginidae@gmail.com>
2019-02-15 16:22:42 -0600

    tests for users existing in db

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,27 @@
-/* global describe, beforeEach */
+/* global describe, beforeEach, it */
+/* eslint-disable no-unused-expressions */
 require('dotenv').config({ path: './.env' });
+const { expect } = require('chai');
 const { user, event, comment } = require('../server/models/index');
 const db = require('../server/db/config');
-const should = require('chai').should();
 
 describe('db', () => {
+  // tests db methods in isolation of server
   beforeEach((done) => {
-    Promise.all(Object.values(db).map(model => model.truncate()))
+    Promise.all([db.User, db.Event, db.InterestedEvent]
+      .map(model => model.truncate({ cascade: true })))
       .then(() => Promise.all([user.save({ username: 'alexa', password: 1234 }), event.save({ category: 'ham', title: 'spam', description: 'ham and spam' })]))
-      .then(([newUser, newEvent]) => user.followEvent({ userId: newUser.id, newEvent }))
-      .then(() => done());
+      .then(([newUser, newEvent]) => user.followEvent(newUser.id, newEvent))
+      .then(() => done())
+      .catch(err => console.error(err));
+  });
+
+  it('should store users in the database', (done) => {
+    db.User.find({})
+      .then((queryResult) => {
+        expect(queryResult).to.exist;
+        expect(queryResult.username).to.equal('alexa');
+        done();
+      });
   });
 });
