4d5005c6a4f1466b78b0bc33d55cab2c8ae540f4
aeginidae <aeginidae@gmail.com>
2019-02-15 16:57:55 -0600

    tests if users are removed

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,27 +1,44 @@
-/* global describe, beforeEach, it */
+/* global describe, beforeEach, afterEach, it */
 /* eslint-disable no-unused-expressions */
+/* eslint-disable no-shadow */
 require('dotenv').config({ path: './.env' });
 const { expect } = require('chai');
-const { user, event, comment } = require('../server/models/index');
+const { user, event } = require('../server/models/index');
 const db = require('../server/db/config');
 
 describe('db', () => {
   // tests db methods in isolation of server
-  beforeEach((done) => {
-    Promise.all([db.User, db.Event, db.InterestedEvent]
-      .map(model => model.truncate({ cascade: true })))
-      .then(() => Promise.all([user.save({ username: 'alexa', password: 1234 }), event.save({ category: 'ham', title: 'spam', description: 'ham and spam' })]))
-      .then(([newUser, newEvent]) => user.followEvent(newUser.id, newEvent))
-      .then(() => done())
-      .catch(err => console.error(err));
-  });
-
-  it('should store users in the database', (done) => {
-    db.User.find({})
-      .then((queryResult) => {
-        expect(queryResult).to.exist;
-        expect(queryResult.username).to.equal('alexa');
-        done();
-      });
+  describe('User', () => {
+    beforeEach((done) => {
+      user.save({ id: 1, username: 'alexa', password: '1234' })
+        .then(() => done())
+        .catch(err => done(err));
+    });
+    afterEach((done) => {
+      user.delete(1)
+        .then(() => done())
+        .catch(err => done(err));
+    });
+    it('should store users in the database', (done) => {
+      db.User.find({})
+        .then((queryResult) => {
+          expect(queryResult).to.exist;
+          expect(queryResult.username).to.equal('alexa');
+          done();
+        })
+        .catch(err => done(err));
+    });
+    it('should remove users from the database', (done) => {
+      user.delete(1)
+        .then((removed) => {
+          expect(removed).to.equal(1);
+          return db.User.findAndCountAll({});
+        })
+        .then((queryResult) => {
+          expect(queryResult.count).to.equal(0);
+          done();
+        })
+        .catch(err => done(err));
+    });
   });
 });
