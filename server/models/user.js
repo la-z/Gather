4296d1132c2a07f2174cb288ea58d24311663745
aeginidae <aeginidae@gmail.com>
2019-02-14 19:39:03 -0600

    change userModel to userId

--- server/models/user.js
+++ server/models/user.js
@@ -29,29 +29,38 @@ followEvent
 creates new entry in table InterestedEvent, linking user and event
 initializes "rsvp" to false
 @params
-  userModel: Sequelize.Model
+  userId: Integer
   eventModel: Sequelize.Model
 returns: Promise (new InterestedEvent)
 */
-module.exports.followEvent = (userModel, eventModel) => InterestedEvent.create({
-  user_id: userModel.get('id'),
+module.exports.followEvent = (userId, eventModel) => InterestedEvent.create({
+  user_id: userId,
   event_id: eventModel.get('id'),
   rsvp: false,
 });
+
+/*
+unfollowEvent
+deletes the entry in InterestedEvent corresponding to given user and event
+@params
+  userId: Integer
+  eventModel: Sequelize.Model
+returns: Promise (Integer -- number of rows deleted (should always be 1))
+*/
 
 /*
 rsvpEventToggle
 finds entry in InterestedEvent corresponding to given user and event
 flips InterestedEvent instance's rsvp property, saves to db
 @params
-  userModel: Sequelize.Model
+  userId: Integer
   eventModel: Sequelize.Model
 returns: Promise (Array: number updated rows, updated InterestedEvent)
 */
-module.exports.rsvpEventToggle = (userModel, eventModel) => InterestedEvent.update({ rsvp: Sequelize.literal('NOT rsvp') },
+module.exports.rsvpEventToggle = (userId, eventModel) => InterestedEvent.update({ rsvp: Sequelize.literal('NOT rsvp') },
   {
     where: {
-      user_id: userModel.id,
+      user_id: userId,
       event_id: eventModel.id,
     },
     returning: true, // forces sequelize to return affected rows with postgres, default false
