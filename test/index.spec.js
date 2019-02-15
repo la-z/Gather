eafb39daea3f8545884e340d85c4549bf3780744
aeginidae <aeginidae@gmail.com>
2019-02-15 14:50:45 -0600

    require db config and models

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,13 @@
-/* global describe */
+/* global describe, beforeEach */
+require('dotenv').config({ path: './.env' });
+const { user, event, comment } = require('../server/models/index');
+const db = require('../server/db/config');
 const should = require('chai').should();
 
 describe('db', () => {
+
+  beforeEach((done) => {
+
+  });
 
 });
