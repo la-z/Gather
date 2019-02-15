18511caa00c21b94392f3a59dc8b3b445aaf4145
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-02-15 17:19:52 -0600

    Add EventListEntry

--- react-client/src/components/eventList.jsx
+++ react-client/src/components/eventList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import EventListEntry from './eventListEntry.jsx'
 const EventList = () => (
   <div>
-    
+    <h2>EventList</h2>
+    <EventListEntry />
   </div>
 );
 export default EventList;
