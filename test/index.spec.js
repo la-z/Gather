ecfb2f244c86075f88d055786aa077c43ce8e889
aeginidae <aeginidae@gmail.com>
2019-02-15 15:01:06 -0600

    define beforeEach hook for db

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,9 +5,10 @@ const db = require('../server/db/config');
 const should = require('chai').should();
 
 describe('db', () => {
-
   beforeEach((done) => {
-
+    Promise.all(Object.values(db).map(model => model.truncate()))
+      .then(() => Promise.all([user.save({ username: 'alexa', password: 1234 }), event.save({ category: 'ham', title: 'spam', description: 'ham and spam' })]))
+      .then(([newUser, newEvent]) => user.followEvent({ userId: newUser.id, newEvent }))
+      .then(() => done());
   });
-
 });
