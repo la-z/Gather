40dd16c1e7ac27a2456c820bf53bdd108447f8bc
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-02-15 15:52:42 -0600

    add merge

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -5,22 +5,11 @@ import axios from 'axios';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
-<<<<<<< HEAD
-  constructor(props){
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  
-=======
   constructor(props) {
     super(props);
     this.state = {};
   }
 
->>>>>>> 2e321a7f0ef4c71d81e294cca87a96582507ed3f
   render() {
     return (
       <h1>Hello World</h1>
