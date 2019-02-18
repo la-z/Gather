852fd3113fbe48f092069b6d7259f7af57997940
aeginidae <aeginidae@gmail.com>
2019-02-17 20:45:29 -0600

    migrate Sequelize initiation to server/models/index.js

---
+++ server/models/index.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const db = {};
+
+const sequelize = new Sequelize('postgres', process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+});
+
+const models = [
+  'user',
+  'event',
+  'comment',
+];
+
+models.forEach((model) => {
+  db[model] = sequelize.import(`${__dirname}/${model}`);
+});
+
+models.forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
