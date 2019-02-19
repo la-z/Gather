ec1baabff84f54e050f7e3feaecd670f4f8a2d30
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-02-19 15:51:22 -0600

    Adds eslint disable for prop-types and fixes other ESLint errors

--- react-client/src/components/eventList.jsx
+++ react-client/src/components/eventList.jsx
@@ -1,9 +1,11 @@
+/* eslint react/prop-types: 0 */
 import React from 'react';
-import EventListEntry from './eventListEntry.jsx'
-const EventList = (props) => (
+import EventListEntry from './eventListEntry';
+
+const EventList = ({ events }) => (
   <div>
     <h2>EventList</h2>
-    {props.events.map((event) => <EventListEntry key={event.title} event={event}/>)}
+    {events.map(event => <EventListEntry key={event.title} event={event} />)}
   </div>
 );
 export default EventList;
