41233cdc5ccf9386329167be088a683610dc10df
aeginidae <aeginidae@gmail.com>
2019-02-18 23:18:41 -0600

    instanceMethods no longer works

--- server/models/event.js
+++ server/models/event.js
@@ -7,19 +7,17 @@ module.exports = (sequelize, DataTypes) => {
     time: DataTypes.DATE,
     lat: DataTypes.NUMERIC,
     long: DataTypes.NUMERIC,
-  }, {
-    instanceMethods: {
-      // instanceMethods are attached by sequelize to each instance of a model
-      togglePrivate() {
-        this.update({ private: !this.get('private') });
-      },
-    },
   });
+
+  Event.prototype.togglePrivate = () => {
+    this.update({ private: !this.get('private') });
+  };
 
   Event.associate = (models) => {
     Event.belongsToMany(models.User, { through: models.InterestedEvent });
     // this gives instances of Event the methods getUsers, setUsers, addUser, and addUsers
     Event.hasOne(models.User);
   };
+
   return Event;
 };
