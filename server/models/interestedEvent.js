66777dedd449fe51772b4b7d52e93cc743480cc3
aeginidae <aeginidae@gmail.com>
2019-02-18 23:19:48 -0600

    instanceMethods no longer works

--- server/models/interestedEvent.js
+++ server/models/interestedEvent.js
@@ -1,12 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const InterestedEvent = sequelize.define('InterestedEvent', {
     rsvp: { type: DataTypes.BOOLEAN, default: false },
-  }, {
-    instanceMethods: {
-      toggleRsvp() {
-        this.update({ rsvp: !this.get('rsvp') });
-      },
-    },
   });
+
+  InterestedEvent.prototype.toggleRsvp = () => {
+    this.update({ rsvp: !this.get('rsvp') });
+  };
+
   return InterestedEvent;
 };
