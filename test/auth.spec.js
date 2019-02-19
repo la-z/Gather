178816cd4e4839d4a196c84fd1db5ea1ba544e71
aeginidae <aeginidae@gmail.com>
2019-02-19 16:08:42 -0600

    add test for new users stored in db

---
+++ test/auth.spec.js
@@ -0,0 +1,54 @@
+/* global describe, context, before, beforeEach, afterEach, it */
+/* eslint-disable no-unused-expressions */
+
+// these are LIVE TESTS that deal with the actual database!
+// as such, they are subject to some unpredictable behavior
+// if the testing username actually becomes taken, the tests will fail to function
+const request = require('supertest');
+const chai = require('chai');
+const app = require('../server/server');
+const db = require('../server/models');
+
+const { expect } = chai;
+
+const newUser = {
+  username: 'abcdefg123',
+  password: 'hellothisispassword',
+};
+
+describe('signup', () => {
+  beforeEach((done) => {
+    request(app)
+      .post('/signup')
+      .send(newUser)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(done);
+  });
+
+  afterEach((done) => {
+    db.User.destroy({ username: newUser.username })
+      .then(done);
+  });
+
+  it('should store a new user in the db with a hashed password', (done) => {
+    request(app)
+      .post('/signup')
+      .send(newUser)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(() => {
+        db.User.findOne({ username: newUser.username })
+          .then(foundUser => foundUser.checkPassword(newUser.password, foundUser.password))
+          .then((isValidPass) => {
+            expect(isValidPass).to.be.true;
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+});
+
+describe('sessions', () => {
+
+});
