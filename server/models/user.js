1d25230811428b87308edf95a08210e43aa4eb60
aeginidae <aeginidae@gmail.com>
2019-02-19 15:18:11 -0600

    User password is hashed before db storage

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,6 @@
+/* eslint-disable func-names */
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
@@ -11,5 +14,17 @@ module.exports = (sequelize, DataTypes) => {
     // this gives instances of User the methods getEvents, setEvents, addEvent, and addEvents
     User.hasMany(models.Event);
   };
+
+  User.prototype.hashPassword = function (plainTextPassword) {
+    bcrypt.hash(plainTextPassword, 10)
+    // 10 is for complexity of salt generated
+      .then((hash) => {
+        this.password = hash;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return User;
 };
