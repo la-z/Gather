025c8628aef10a132e6e638a72cfa96fc53efb76
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-02-19 16:52:58 -0600

    Fixes some eslint errors

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,28 +1,28 @@
 /* eslint import/extensions: 0 */
-import events from './mockEvents.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import events from './mockEvents.js';
 import Navbar from './components/navbar.jsx';
 import Categories from './components/categories.jsx';
 import EventList from './components/eventList.jsx';
-import axios from 'axios';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: events,
+      events,
     };
   }
 
   render() {
-  return (
+    return (
       <div>
         <h1>Gather</h1>
         <Navbar />
         <Categories />
-        <EventList events={this.state.events}/>
+        <EventList events={this.state.events} />
       </div>
     );
   }
