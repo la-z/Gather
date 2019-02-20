24b9c3b23da160429ac6dc669142ae592240aebc
aeginidae <aeginidae@gmail.com>
2019-02-20 14:32:29 -0600

    exports passport config

--- server/helpers/session.js
+++ server/helpers/session.js
@@ -1,24 +1,25 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const db = require('../models');
 
-passport.use(new LocalStrategy((username, password, done) => {
+module.exports = (passport) => {
+  passport.use(new LocalStrategy((username, password, done) => {
   // passport needs to "use" a "strategy" http://www.passportjs.org/docs/configure/
-  db.User.findOne({ username })
-    .then(user => user.checkPassword(password))
-    .then((isValid) => {
-      if (isValid) {
-        return done(null, user);
-      }
-      return done(null, false, { message: 'Incorrect username or password.' });
-    })
-    .catch(err => done(err));
-}));
+    db.User.findOne({ where: { username } })
+      .then(user => user.checkPassword(password))
+      .then((isValid) => {
+        if (isValid) {
+          return done(null, user);
+        }
+        return done(null, false, { message: 'Incorrect username or password.' });
+      })
+      .catch(err => done(err));
+  }));
 
-passport.serializeUser((user, done) => done(null, user.id));
-// saves user id on session
+  passport.serializeUser((user, done) => done(null, user.id));
+  // saves user id on session
 
-passport.deserializeUser((id, done) => {
-  db.User.findById(id, done);
-});
-// associates session with user
+  passport.deserializeUser((id, done) => {
+    db.User.findById(id, done);
+  });
+  // associates session with user
+};
