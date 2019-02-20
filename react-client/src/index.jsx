d141d090403758e03a2073d124752cdf6a60718a
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-02-20 10:57:01 -0600

    Adds different rendering depending on if event is clicked

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,28 +1,48 @@
 /* eslint import/extensions: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import events from './mockEvents.js';
+// import axios from 'axios';
+import data from './mockEvents.js';
 import Navbar from './components/navbar.jsx';
 import Categories from './components/categories.jsx';
 import EventList from './components/eventList.jsx';
+import currentlyClickedEvent from './components/CurrentlyClickedEvent.jsx';
 // import PropTypes from 'prop-types';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      events,
+      events: data,
+      clickedEvent: null,
     };
   }
 
+  renderClickedEventTitle(object) {
+    this.setState({
+      clickedEvent: object,
+    });
+  }
+
   render() {
+    const { events, clickedEvent } = this.state;
     return (
       <div>
         <h1>Gather</h1>
-        <Navbar />
-        <Categories />
-        <EventList events={this.state.events} />
+        {
+          clickedEvent !== null ? (
+            <div>
+              <Navbar />
+              <currentlyClickedEvent event={clickedEvent} />
+            </div>
+          )
+            : (
+              <div>
+                <Categories />
+                <EventList events={events} renderClickedEventTitle={this.renderClickedEventTitle} />
+              </div>
+            )
+        }
       </div>
     );
   }
