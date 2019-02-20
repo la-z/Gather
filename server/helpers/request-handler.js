e7f329f994d9b451fcf2cad55fe5e973c547c236
aeginidae <aeginidae@gmail.com>
2019-02-20 13:48:58 -0600

    rework request-handler

--- server/helpers/request-handler.js
+++ server/helpers/request-handler.js
@@ -1,4 +1,46 @@
-module.exports.requestHandler = (req, res, next) => {
-  res.send('Hello World');
-  next();
+const requestHandler = {
+  login(req, res) {
+
+  },
+  logout(req, res) {
+
+  },
+  signup(req, res) {
+
+  },
+  getEvent(req, res) {
+
+  },
+  getCategory(req, res) {
+
+  },
+  getProfile(req, res) {
+
+  },
+  getNewEvent(req, res) {
+
+  },
+  makeNewEvent(req, res) {
+
+  },
+  submitNewComment(req, res) {
+
+  },
+  deleteComment(req, res) {
+
+  },
+  editComment(req, res) {
+
+  },
+  deleteEvent(req, res) {
+
+  },
+  editEvent(req, res) {
+
+  },
+  deleteUser(req, res) {
+
+  },
 };
+
+module.exports = requestHandler;
