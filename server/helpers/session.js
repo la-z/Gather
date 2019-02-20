6fb8c055211f5cfbc2900e935426cb40e5b9681f
aeginidae <aeginidae@gmail.com>
2019-02-19 23:33:15 -0600

    passport stores and reads user id on session

--- server/helpers/session.js
+++ server/helpers/session.js
@@ -1,11 +1,10 @@
-const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const db = require('../models');
 
 passport.use(new LocalStrategy((username, password, done) => {
   // passport needs to "use" a "strategy" http://www.passportjs.org/docs/configure/
-  db.Users.findOne({ username })
+  db.User.findOne({ username })
     .then(user => user.checkPassword(password))
     .then((isValid) => {
       if (isValid) {
@@ -15,3 +14,11 @@ passport.use(new LocalStrategy((username, password, done) => {
     })
     .catch(err => done(err));
 }));
+
+passport.serializeUser((user, done) => done(null, user.id));
+// saves user id on session
+
+passport.deserializeUser((id, done) => {
+  db.User.findById(id, done);
+});
+// associates session with user
