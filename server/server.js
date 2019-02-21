7b89015b448352db1d25bfa679efd43fc1c9d9a3
aeginidae <aeginidae@gmail.com>
2019-02-21 12:23:20 -0600

    tentatively catches all stray requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const session = require('express-session');
 require('./helpers/session')(passport);
 
 const PORT = process.env.PORT || 1128;
@@ -11,15 +12,28 @@ const requestHandler = require('./helpers/request-handler');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(session({ secret: '29187264' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '../react-client/dist')));
 app.get('/insertSomethingHere', (req, res, next) => {
   requestHandler(req, res, next);
 });
+
 app.post('/login', passport.authenticate('local', {
   failureRedirect: '/',
   failureFlash: true,
 }), (req, res) => res.redirect(`/${req.user.username}/profile`));
+
+app.get('/logout', requestHandler.logout);
+
+app.post('/signup', requestHandler.signup);
+
+app.all('*', (req, res) => {
+  console.log('idk what happened');
+  res.send(500, 'That was strange');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
