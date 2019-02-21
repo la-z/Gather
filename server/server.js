8c235eeee3e3394bae70896864db75c30296143a
aeginidae <aeginidae@gmail.com>
2019-02-21 14:07:58 -0600

    user can sign up and log in

--- server/server.js
+++ server/server.js
@@ -8,23 +8,26 @@ require('./helpers/session')(passport);
 
 const PORT = process.env.PORT || 1128;
 const requestHandler = require('./helpers/request-handler');
+const { checkAuthentication } = require('./helpers/auth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(session({ secret: '29187264' }));
+// literally just random numbers
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '../react-client/dist')));
-app.get('/insertSomethingHere', (req, res, next) => {
-  requestHandler(req, res, next);
-});
+
+app.get('/users/:username/profile', checkAuthentication, requestHandler.getProfile);
 
 app.post('/login', passport.authenticate('local', {
   failureRedirect: '/',
   failureFlash: true,
-}), (req, res) => res.redirect(`/${req.user.username}/profile`));
+}), (req, res) => {
+  res.redirect(`/users/${req.user.username}/profile`);
+});
 
 app.get('/logout', requestHandler.logout);
 
