06d40e50ddc2402edadf166a5a05ada464054a58
aeginidae <aeginidae@gmail.com>
2019-02-21 12:20:54 -0600

    handles duplicate username

--- server/helpers/request-handler.js
+++ server/helpers/request-handler.js
@@ -1,12 +1,26 @@
 /* eslint-disable no-unused-expressions */
-
+const db = require('../models');
 
 const requestHandler = {
   logout(req, res) {
 
   },
-  signup(req, res) {
-
+  signup(req, res, next) {
+    const newUser = req.body;
+    db.User.create(newUser)
+      .then((returnedUser) => {
+        req.login({ username: returnedUser.username, id: returnedUser.id }, (err) => {
+          if (err) return next(err);
+          return res.redirect(`/${req.user.username}/profile`);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.message === 'Validation error') {
+          res.redirect('/');
+        }
+        res.send(500, 'Something went wrong on our end.');
+      });
   },
   getEvent(req, res) {
 
