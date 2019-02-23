cd18b4aacefab68319dc1b06837987929b10eccd
cskladz <cskladzien@gmail.com>
2019-02-23 16:52:12 -0600

    SignUp Form makes post to server with form data

--- react-client/src/components/SignupForm.jsx
+++ react-client/src/components/SignupForm.jsx
@@ -1,59 +1,62 @@
 import React from 'react';
 import { Button } from 'react-materialize';
+import axios from 'axios';
 
 class SignupForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      username: '',
+      password: '',
+      email: '',
+      tel: '',
     };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleTelChange = this.handleTelChange.bind(this);
+    this.handleSignup = this.handleSignup.bind(this);
   }
 
-  // onSubmit,
-  // onChange,
+  handleUsernameChange(e) {
+    this.setState({ username: e.target.value });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handleTelChange(e) {
+    this.setState({ tel: e.target.value });
+  }
+
+  handleSignup() {
+    const { username, password, email, tel } = this.state;
+    const params = {
+      username,
+      password,
+      email,
+      tel,
+    };
+    axios.post('/signup', params)
+      .then((result) => { console.log(result); })
+      .catch((err) => { console.log(err); });
+  }
+
+
   render() {
+    const { username, password, email, tel } = this.state;
     return (
-      <form className="form-inline">
-        {/* onSubmit={this.handleFormSubmit}> */}
-      username: (Minimum requirements)
-        <input
-          type="text"
-          className="form-control input-lg"
-          id="username"
-          placeholder="username"
-          // ref={this.setSearchInputElementReference}
-          // required
-        />
-        password:
-        <input
-          type="text"
-          className="form-control input-lg"
-          id="password"
-          placeholder="******"
-          // ref={this.setSearchInputElementReference}
-          // required
-        />
-        Confirm password:
-        <input
-          type="text"
-          className="form-control input-lg"
-          id="passwordConfirm"
-          placeholder="******"
-          // ref={this.setSearchInputElementReference}
-          // required
-        />
-        Email
-        <input
-          type="text"
-          className="form-control input-lg"
-          id="password"
-          placeholder="******"
-          // ref={this.setSearchInputElementReference}
-          // required
-        />
-        <Button type="submit">
-          Submit
-        </Button>
+      <form>
+        <input type="text" name="text" placeholder="username" value={username} onChange={this.handleUsernameChange} />
+        <input type="text" name="password" placeholder="Password" value={password} onChange={this.handlePasswordChange} />
+        <input type="text" name="email" placeholder="Email" value={email} onChange={this.handleEmailChange} />
+        <input type="tel" name="tel" placeholder="504555555" value={tel} onChange={this.handleTelChange} />
+        <Button type="button" onClick={this.handleSignUp}>Sign Up!!</Button>
       </form>
     );
   }
