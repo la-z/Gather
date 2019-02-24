aaf3eb8066e257459de4daa3db4c62fd399328f6
aeginidae <aeginidae@gmail.com>
2019-02-24 13:00:58 -0600

    unique email

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-    email: DataTypes.STRING,
+    email: { type: DataTypes.STRING, unique: true },
     telephone: DataTypes.STRING,
   });
 
