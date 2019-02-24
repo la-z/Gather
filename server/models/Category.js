35229853af6e1fccfe844c5c5bfc27c579fa41ce
aeginidae <aeginidae@gmail.com>
2019-02-24 13:15:18 -0600

    category name must be unique

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    name: { type: DataTypes.STRING, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false, unique: true },
   });
 
   Category.associate = (models) => {
