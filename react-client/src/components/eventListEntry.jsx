e78b6d953e5d2ea5cfd45a74fd1c18dee593d9d3
aeginidae <aeginidae@gmail.com>
2019-02-24 21:35:17 -0600

    EventListEntry is wrapped in Col

--- react-client/src/components/eventListEntry.jsx
+++ react-client/src/components/eventListEntry.jsx
@@ -2,14 +2,16 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint react/prop-types: 0 */
 import React from 'react';
-import { CardPanel } from 'react-materialize';
+import { Card, Col } from 'react-materialize';
 
 const EventListEntry = ({ event, renderClickedEventTitle }) => (
-  <CardPanel>
-    <h4 onClick={() => renderClickedEventTitle(event)}>{event.title}</h4>
-    <h4>{event.category}</h4>
-    <p>{event.description}</p>
-    <p>{event.time}</p>
-  </CardPanel>
+  <Col s={12} m={4}>
+    <Card className="card">
+      <h4 onClick={() => renderClickedEventTitle(event)}>{event.title}</h4>
+      <h4>{event.category}</h4>
+      <p>{event.description}</p>
+      <p>{event.time}</p>
+    </Card>
+  </Col>
 );
 export default EventListEntry;
