6d066a154378c20829e90c5b77b54eb0e81409d8
aeginidae <aeginidae@gmail.com>
2019-02-24 21:25:47 -0600

    EventListEntry is CardPanel

--- react-client/src/components/eventListEntry.jsx
+++ react-client/src/components/eventListEntry.jsx
@@ -2,16 +2,14 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint react/prop-types: 0 */
 import React from 'react';
+import { CardPanel } from 'react-materialize';
 
 const EventListEntry = ({ event, renderClickedEventTitle }) => (
-  <div>
-    <h3 onClick={() => renderClickedEventTitle(event)}>{event.title}</h3>
+  <CardPanel>
+    <h4 onClick={() => renderClickedEventTitle(event)}>{event.title}</h4>
     <h4>{event.category}</h4>
     <p>{event.description}</p>
     <p>{event.time}</p>
-    {
-      event.private ? <p>This is a private Event</p> : <p>This is NOT a private Event</p>
-    }
-  </div>
+  </CardPanel>
 );
 export default EventListEntry;
