03991de8943c1901781025c6708cc0ac052139d5
aeginidae <aeginidae@gmail.com>
2019-02-24 23:30:56 -0600

    categories view is narrower

--- react-client/src/components/categories.jsx
+++ react-client/src/components/categories.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Collection, CollectionItem } from 'react-materialize';
+import { Collection, CollectionItem, Col } from 'react-materialize';
+import Row from 'react-materialize/lib/Row';
 
 const Categories = ({ categories, getCategory }) => (
-  <Collection>
-    {categories.map(category => <CollectionItem onClick={() => getCategory(category.id)}>{category.name}</CollectionItem>)}
-  </Collection>
+  <Row>
+    <Col s={12} m={4}>
+      <Collection header="Categories">
+        {categories.map(category => <CollectionItem onClick={() => getCategory(category.id)}>{category.name}</CollectionItem>)}
+      </Collection>
+    </Col>
+  </Row>
 );
 export default Categories;
