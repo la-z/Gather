b0b3921e466f34c6ec8dfc0b351f87f75bb68440
aeginidae <aeginidae@gmail.com>
2019-02-25 19:20:45 -0600

    clean CommentForm

--- react-client/src/components/CommentForm.jsx
+++ react-client/src/components/CommentForm.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Button, Toast } from 'react-materialize';
+import { Button } from 'react-materialize';
 import axios from 'axios';
 
 class CommentForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: this.props.username,
-      eventID: this.props.eventID, 
-      comment: '', //this is the value from the field
+      comment: '', // this is the value from the field
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleCommentChange = this.handleCommentChange.bind(this);
   }
+
   handleCommentChange(e) {
     this.setState({
       comment: e.target.value,
@@ -20,29 +19,31 @@ class CommentForm extends React.Component {
   }
 
   handleSubmit(e) {
+    const { username, eventID, refresh } = this.props;
+    const { comment } = this.state;
     e.preventDefault();
     // put in an ajax request to the server to store the data in the database
-    console.log(this.state);
-    if (!this.state.username) {
+    if (!username) {
       window.Materialize.toast('Please Login to Comment!', 1000);
     } else {
-      const { comment, eventID } = this.state;
       const params = {
         body: comment,
-      }
+      };
       axios.put(`/events/${eventID}/comments`, params)
-        .then((response) => { 
-          this.props.redirect();
-          console.log(response); 
+        .then((response) => {
+          refresh();
+          console.log(response);
         });
     }
   }
 
   render() {
+    const { handleSubmit, handleCommentChange } = this.props;
+    const { comment } = this.state;
     return (
-      <form className="comment-form" onSubmit={this.handleSubmit}>        
+      <form className="comment-form" onSubmit={handleSubmit}>
         <span>Comment</span>
-        <input type="text" placeholder="Say something..."  value={this.state.comment} className="form-control" onChange={ this.handleCommentChange }/>
+        <input type="text" placeholder="Say something..." value={comment} className="form-control" onChange={handleCommentChange} />
         <Button type="submit" className="btn btn-primary"> Post Comment! </Button>
       </form>
     );
