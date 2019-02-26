d5be42e12b9ff3cdf83ca9e76cc0d82ebf8fd708
aeginidae <aeginidae@gmail.com>
2019-02-25 19:18:30 -0600

    add browser env

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,9 @@ module.exports = {
         "no-console": "off",
         "import/extensions": "off",
         "react/prop-types": "off",
+    },
+    "env": {
+        "browser": true,
+        "node": true,
     }
 };
