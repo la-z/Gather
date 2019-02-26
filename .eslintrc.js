3d0df39466f9a6aff7aba061f5daeb3ca1654337
aeginidae <aeginidae@gmail.com>
2019-02-25 19:07:14 -0600

    extra rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "extends": "airbnb",
     rules: {
-        "no-console": "off"
+        "no-console": "off",
+        "import/extensions": "off",
+        "react/prop-types": "off",
     }
 };
