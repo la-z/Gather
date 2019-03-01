7b9d1ce6910cc2e9a5662f5c05812b0b7b44b156
Akin <akinpds@gmail.com>
2019-03-01 09:44:31 -0600

    User feedback is sent to user after adding a friend

--- react-client/src/components/addFriendForm.jsx
+++ react-client/src/components/addFriendForm.jsx
@@ -11,20 +11,34 @@ class FriendForm extends React.Component {
         };
         // this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.friendToast = this.friendToast.bind(this);
+        this.bothFunc = this.bothFunc.bind(this);
     }
 
     handleUsernameChange(e) {
         this.setState({ username: e.target.value });
     } 
 
+    friendToast(){
+        return window.Materialize.toast('friend added!', 2000);
+    }
+
+    bothFunc(){
+        const { username, password } = this.state;
+        const { handleLogin } = this.props;
+        handleLogin(username, password);
+        this.friendToast()
+    }
+    
 
     render() {
         const { username, password } = this.state;
         const { handleLogin } = this.props;
+        const bothFunc = this.bothFunc;
         return (
             <form>
                 <input type="text" name="text" placeholder="username" value={username} onChange={this.handleUsernameChange} />
-                <Button className="orange darken-3" type="button" onClick={() => handleLogin(username, password)}>Add Friend!!</Button>
+                <Button className="orange darken-3" type="button" onClick={() => {bothFunc(); }   }>Add Friend!!</Button>
             </form>
         );
     }
